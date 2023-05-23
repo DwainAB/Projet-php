f106c1676430f4b73f6199b66003a98f99012b99
dwain93 <74243765+dwain93@users.noreply.github.com>
2023-05-24 01:25:44 +0200

    Ajout de la page de log

--- src/Components/Log/Log.jsx
+++ src/Components/Log/Log.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import line from "../../Assets/line.svg"
+import logo from "../../Assets/logo.png"
 import "./Log.css"
 
 function Log(){
+
+    useEffect(() => {
+        function flip() {
+          let formSignup = document.querySelector('.container-form-signup');
+          let formLogin = document.querySelector('.container-form-login');
+          let flipButton = document.querySelector('.flip-button')
+    
+          if (formSignup.classList.contains('flip-active')) {
+            formSignup.classList.remove('flip-active');
+            formLogin.classList.add('flip-active');
+            flipButton.innerHTML = "Identifiez vous"
+          } else {
+            formLogin.classList.remove('flip-active');
+            formSignup.classList.add('flip-active');
+            flipButton.innerHTML = "Créer votre compte Restchat"
+          }
+        }
+    
+        const buttonFlip = document.querySelector('.flip-button');
+        buttonFlip.addEventListener('click', flip);
+    
+        return () => {
+          buttonFlip.removeEventListener('click', flip);
+        };
+      }, []);
+    
     return(
         <div className="container-log">
-            <div className="container-form-signup">
+            <img className="logo-form" src={logo} alt="" />
+            <div className="container-form-signup flip-active">
                 <h1>Inscription</h1>
                 <form className="form-signup" action="POST">
                     <input type="text"  placeholder="Nom d'utilisateur"/>
@@ -21,6 +50,11 @@ function Log(){
                     <input type="password" placeholder="Mot de passe" />
                     <input className="btn-form" type="submit" value="Connexion"/>
                 </form>
+            </div>
+
+            <div className="container-flip">
+                <p className="flip-text"><img src={line} alt="" />Nouveau chez Reschat<img src={line} alt="" /></p>
+                <button className="flip-button">Créer votre compte Restchat</button>
             </div>
         </div>
     )
