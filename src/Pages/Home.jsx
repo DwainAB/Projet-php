bea5a75631277c01a23c2ee8f1867bbe9fd24589
Aghiles Haddar <haddaraghiles5@gmail.com>
2023-05-23 23:12:18 +0200

    home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,34 @@
 import React from "react";
+import Addlogo from '../../src/Assets/add-logo.svg'
+import Profil1 from '../../src/Assets/profil-1.png'
+import Profil2 from '../../src/Assets/profil-2.png'
+
+
 
 function Home(){
     return(
-      <div>
-        <h1>Home</h1>
-        <p>est</p>
-        <div className="svg-1"></div>
-        <div className="svg-2"></div>
-        <div className="svg-3"></div>
-        <div className="svg-4"></div>
+      <div className="container-middle">
+
+        <div className="middle-first">
+            <div className="middle-profil-story">
+              <img className="logo-add-svg" src={Addlogo} alt="" />
+              <img className="profil-img" src={Profil1} alt="" />
+              <img className="profil-img" src={Profil2} alt="" />
+            </div>
+        </div>
+
+
+
+
+
+        <div className="svg-all">  
+          <div className="svg-1"></div>
+          <div className="svg-2"></div>
+          <div className="svg-3"></div>
+          <div className="svg-4"></div>
+        </div>
       </div>
+      
     )
 }
 
